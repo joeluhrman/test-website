ac0a6eb601cf799601ee664dffb2e81ddfd242ba
Joe Luhrman <jaluhrman2018@gmail.com>
2022-02-25 12:32:35 -0500

    yuh

--- test-website.go
+++ test-website.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, _ := os.ReadFile(filename)
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/"):]
+	p, _ := loadPage(title)
+	t, _ := template.ParseFiles("homepage.html")
+	t.Execute(w, p)
+}
+
+func main() {
+	//var p1 *Page = loadPage("page")
+	//fmt.Println(p1.Title + "\n" + string(p1.Body))
+
+	http.HandleFunc("/", viewHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
