9dd93bf0eddcbeafb1b8da8a5df1f5ddf97b5ec2
Joe Luhrman <jaluhrman2018@gmail.com>
2022-02-25 21:16:12 -0500

    j

--- test-website.go
+++ test-website.go
@@ -1,15 +1,51 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"os"
+)
+
+type MuscleGroup string
+
+const (
+	Legs MuscleGroup = "Legs"
+	Push MuscleGroup = "Push"
+	Pull MuscleGroup = "Pull"
 )
 
 type Page struct {
 	Title string
 	Body  []byte
+}
+
+type ExerciseType struct {
+	Name   string
+	BdyPrt MuscleGroup
+}
+
+type Exercise struct {
+	Type  ExerciseType
+	Sets  int
+	Reps  int
+	Notes string
+}
+
+type Workout struct {
+	Date      string
+	Exercises []Exercise
+	Notes     string
+}
+
+func newExercise(name string, sets int, reps int, notes string) *Exercise {
+	exercise := Exercise{Name: name, Sets: sets, Reps: reps, Notes: notes}
+	return &exercise
+}
+
+func newWorkout(date string, exercises []Exercise, notes string) *Workout {
+	workout := Workout{Date: date, Exercises: exercises, Notes: notes}
+	return &workout
 }
 
 func loadPage(title string) (*Page, error) {
@@ -26,9 +62,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	//var p1 *Page = loadPage("page")
-	//fmt.Println(p1.Title + "\n" + string(p1.Body))
+	//http.HandleFunc("/", viewHandler)
+	//log.Fatal(http.ListenAndServe(":8080", nil))
 
-	http.HandleFunc("/", viewHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	exercise1 := newExercise("Squat", 3, 5, "good")
+	exercise2 := newExercise("Bench", 3, 5, "yes")
+	exercise3 := newExercise("Deadlift", 1, 5, "oh")
+
+	exercises := make([]Exercise, 3, 3)
+	exercises[0] = *exercise1
+	exercises[1] = *exercise2
+	exercises[2] = *exercise3
+
+	workout := Workout{Date: "2/25/2022", Exercises: exercises, Notes: "yuh"}
+
+	//fmt.Println(exercise.Name, exercise.Sets, exercise.Reps)
+	fmt.Println(workout.Date, workout.Exercises, workout.Notes)
 }
